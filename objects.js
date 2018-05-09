9251dc7e34fe5655cceade0284f3b7978d27de09
shandamckinney <mckinney.shanda@gmail.com>
2018-05-09 16:43:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Adele: "Someone like you"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist['artistName']= songTitle
+  playlist[artistName]= songTitle
   return playlist
 }
